ea5c40c23c8e593834b0452301be90cd72631ff5
Ilkhomidin Bakhoraliev <bakhoraliev.ilkhomidin@proton.me>
2024-02-14 01:08:52 +0500

    feature: add base types for invoices

---
+++ source/invoices.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+from typing import Any, TypedDict
+
+
+class Customer(TypedDict):
+    """
+    Customer of the invoice.
+
+    The customer must provide an email address and ip, but in some cases,
+        may also provide another information such as name, address, etc.
+    """
+
+    ip: str
+    email: str
+
+
+class Money(TypedDict):
+    """Amount and currency of the invoice."""
+
+    amount: Decimal
+    currency: str
+
+
+class Invoice(TypedDict):
+    """Invoice to be paid."""
+
+    provider: str
+    method: str
+    amount: Money
+    customer: Customer
+    items: list[str]
+    meta: dict[str, Any]
+    notifications: list[dict[str, Any]]
